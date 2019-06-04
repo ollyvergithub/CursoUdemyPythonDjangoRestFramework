fed5dc8e6f09522d72d49cf4102426926ba2b14f
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-06-04 10:27:27 -0300

    Aula - 4 - 23 - Implementando suas próprias actions personalizadas no ModelViewSet

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -4,6 +4,7 @@
 A viewset that provides default `create()`, `retrieve()`, `update()`,
 `partial_update()`, `destroy()` and `list()` actions.
 """
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
@@ -37,4 +38,13 @@ class PontoTuristicoViewSet(ModelViewSet):
     def partial_update(self, request, *args, **kwargs):
         pass
 
+    # Criando uma action personalizada para um ponto turistico unico
+    @action(detail=True, methods=['get'])
+    def denunciar(self, request, pk=None):
+        pass
+
+    # Criando uma action personalizada para o endpoint geral - EX: Excluir Todos. Method = DELETE
+    @action(detail=False, methods=['get'])
+    def teste(self, request, pk=None):
+        pass
 
