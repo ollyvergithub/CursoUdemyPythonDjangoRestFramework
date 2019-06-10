d88b23a79eb4f9f240fe515fa2ef2ba49a2f17f3
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-06-10 09:21:41 -0300

    Aula - 8 - 30 - Entendendo o mecanismo de autorização

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -8,6 +8,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
