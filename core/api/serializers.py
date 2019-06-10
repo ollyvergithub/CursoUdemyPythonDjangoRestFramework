2b8e4915602204e8833c4d7e07a800c5356d2052
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-06-10 10:17:14 -0300

    Aula - 8 - 31 - 32 - Incrementando um objeto com NestedRelationships

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -2,8 +2,17 @@
 # Serializers define the API representation.
 from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
+from atracoes.api.serializers import AtracoesSerializer
+from enderecos.api.serializers import EnderecosSerializer
 
 class PontoTuristicoSerializer(ModelSerializer):
+    # Aula 9 - 32 - Incrementando um objeto com NestedRelationships
+    # Aqui um relacionamento de muitos para muitos - many=True
+    atracoes = AtracoesSerializer(many=True)
+    # Aqui um relacionamento simples
+    enderecos = EnderecosSerializer()
+
     class Meta:
         model = PontoTuristico
-        fields = ('id','nome', 'descricao', 'aprovado', 'foto')
+        fields = ('id','nome', 'descricao', 'aprovado', 'foto',
+                  'enderecos', 'atracoes', 'comentarios', 'avaliacoes')
